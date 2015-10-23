4d0455e4f401092e0f7ea8b3d73dc6b4ce25b82b
Alexey Melnichuk <mimir@newmail.ru>
2015-10-23 12:10:53 +0300

    Add. `SERVICE` Lua module. This module allows run same code as service and as standalone script.

---
+++ LuaService/Samples/Ticker2/init.lua
@@ -0,0 +1,6 @@
+-- init.lua for the Ticker service
+return {
+	tracelevel = 0,			-- Framework trace level
+	name = "TickService2",	-- Service name for SCM
+	script = "test.lua",	-- Script that runs the service
+}
---
+++ LuaService/Samples/Ticker2/test.lua
@@ -0,0 +1,41 @@
+-- Ticker service implementation
+--[[----------------------------
+This service simply emits a string on the debug console once 
+every 5 seconds until it is stopped. Not necessarily the most
+useful service, but it demonstrates how to construct a service
+in the LuaService framework without too many other system
+assumptions.
+
+Copy the LuaService executable to this folder.
+
+To use sc.exe to create, start, and stop the service:
+
+sc create TickService binPath= "C:\path\to\this\folder\LuaService.exe"
+sc start TickService
+sc stop TickService
+
+To use LuaService to create, start, and stop the service:
+
+LuaService -i		Create and start the ticker service
+LuaService -r 		Start the service
+LuaService -s		Stop the service
+LuaService -u		Uninstall the service
+
+You will want a debug console that is listening to OutputDebugString() to 
+see this service do anything at all. DebugView from www.sysinternals.com
+is a good choice.
+--]]----------------------------
+local SERVICE = require "./SERVICE"
+
+SERVICE.print("Ticker service started, named ", SERVICE.name)
+
+local i = 0						-- counter
+
+SERVICE.run(function()
+  i = i + 1						-- count
+  SERVICE.print("tick", i)		-- OutputDebugString
+end, 1000, 5)
+
+SERVICE.print("Ticker service stopped.")
+
+SERVICE.exit(0)
